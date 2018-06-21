ebb407811b92f63cd63c33a44a9fc0efc0c24236
Alex Felipe <alexfelipevieira@gmail.com>
2018-06-21 10:18:50 -0300

    vídeo 8.1

--- app/src/test/java/br/com/alura/leilao/ExampleUnitTest.java
+++ app/src/test/java/br/com/alura/leilao/ExampleUnitTest.java
@@ -2,6 +2,9 @@ package br.com.alura.leilao;
 
 import org.junit.Test;
 
+import br.com.alura.leilao.model.Usuario;
+
+import static org.hamcrest.CoreMatchers.equalTo;
 import static org.junit.Assert.*;
 
 /**
@@ -13,5 +16,14 @@ public class ExampleUnitTest {
     @Test
     public void addition_isCorrect() {
         assertEquals(4, 2 + 2);
+        assertNotEquals(3, 2 + 2);
+
+        assertTrue(true);
+        assertFalse(false);
+
+        assertNull(null);
+        assertNotNull(new Usuario("Alex"));
+
+        assertThat(2 + 2, equalTo(4));
     }
 }
